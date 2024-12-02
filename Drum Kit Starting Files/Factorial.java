f34f583ed4a7f8ddd16e5809c9fa9ac50200c099
pavan13400 <boyapavankumar6@gmail.com>
2024-12-02 19:17:40 +0530

    drumkit

---
+++ Drum Kit Starting Files/Factorial.java
@@ -0,0 +1,14 @@
+class Factorial{
+    int a=0;
+    int b=1;
+    int c;
+    int d;
+    d=5;
+    for(int i=0;i<=d;i++){
+        System.out.print(c);
+    }
+    c=a+b;
+    a=b;
+    b=c;
+}
+}
---
+++ Drum Kit Starting Files/evenodd.java
@@ -0,0 +1,10 @@
+class evenodd{
+    public static void main(String[]args){
+        int a=11;
+        if(a%2==0){
+            System.out.println(a+" is even");
+        }else{
+            System.out.println(a+" is odd");
+        }
+    }
+}
